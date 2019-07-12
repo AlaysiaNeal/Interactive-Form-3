db7c88c36f1fce597a16d833f6f257b3b8247e98
Alaysia Neal <Alaysia.Neal@verizon.com>
2019-07-12 11:19:44 -0500

    Updated activities new code last one had too many issues
    validation not done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //to make JQuery Work
-$(document).ready(function() {
+$(document).ready(function () {
   //
   $("#name").focus();
 
   //To hide and show the placeholder for other.
   $("#other-title").hide();
 
-  $("#title").change(function() {
+  $("#title").change(function () {
     if ($("#title").val() === "other") {
       $("#other-title").show();
     } else {
@@ -24,7 +24,7 @@ $(document).ready(function() {
   $("#color").prepend(
     '<option selected value="">Please Select A T-Shirt Theme</option>'
   );
-  $("#design").change(function() {
+  $("#design").change(function () {
     $("#color option").hide();
 
     if ($("#design").val() === "js puns") {
@@ -44,79 +44,160 @@ $(document).ready(function() {
 });
 
 
-//This is the function to select workshops and make conflicting options disable.
-$(".activities > label").each(function() {
-  if (($(this).text().indexOf("Tuesday 9am-12pm") >= 0 ) ||
-      ($(this).text().indexOf("Wednesday 9am-12pm") >= 0)) {
-    $(this).children().attr("id", "morningBox");
-
-  } else if(($(this).text().indexOf("Tuesday 1pm-4pm") >= 0) ||  
-            ($(this).text().indexOf("Wednesday 1pm-4pm") >= 0)){
-    $(this).children().attr("id", "afternoonBox");
+ //function used to Select and Deselect the Check Box activities.
+const activities = document.querySelector(`.activities`);
+const checkbox = activities.querySelectorAll(`input[type="checkbox"]`);
+activities.addEventListener("change", () => {
+    if (checkbox[1].checked) {
+        checkbox[3].parentNode.style.color = 'rgba(0,0,0,.2)';
+        checkbox[3].disabled = true;
+    }
     
-  } else {
-    $(this).children().attr("id", "wholeDayBox");
-  }
+    else {
+        checkbox[3].disabled = false;
+        checkbox[3].parentNode.style.color = 'rgba(0,0,0,1)';
+    }
+    if (checkbox[3].checked) {
+        checkbox[1].parentNode.style.color = 'rgba(0,0,0,.2)';
+        checkbox[1].disabled = true;
+    }
+    else {
+        checkbox[1].disabled = false;
+        checkbox[1].parentNode.style.color = 'rgba(0,0,0,1)';
+    }
+    if (checkbox[2].checked) {
+        checkbox[4].parentNode.style.color = 'rgba(0,0,0,.2)';
+        checkbox[4].disabled = true;
+    }
+    else {
+        checkbox[4].disabled = false;
+        checkbox[4].parentNode.style.color = 'rgba(0,0,0,1)';
+    }
+    if (checkbox[4].checked) {
+        checkbox[2].parentNode.style.color = 'rgba(0,0,0,.2)';
+        checkbox[2].disabled = true;
+    }
+    else {
+        checkbox[2].disabled = false;
+        checkbox[2].parentNode.style.color = 'rgba(0,0,0,1)';
+    }
+})
+//Takes the price from a specified individual activity.
+const price = item => {
+    return parseInt(item.substring(item.indexOf('$') + 1));
+}
+const activitySelected = () => {
+    if (totalCost > 0) {
+        return true;
+        console.log('Activity valid'); 
+    } else {
+        
+        return false;
+    }
+}
+const activityErrorDiv = document.createElement('div');
+const activityError = document.createElement('h3');
+const activityTitle = document.querySelector('.activities legend');
+// Add total cost field to activities form.
+let totalCost = 0;
+const activitiesCostDiv = document.createElement('div');
+const activitiesCost = document.createElement('h3');
+activitiesCostDiv.setAttribute('id', 'totalSpend');
+activitiesCostDiv.style.display = 'block';
+activitiesCostDiv.appendChild(activitiesCost);
+activities.appendChild(activitiesCostDiv);
+// Calculate total cost of activities and show the price.
+activities.addEventListener("change", e => {
+    let item = e.target.parentNode.textContent;
+    if (e.target.checked == true) {
+        // If error message is present, remove error once activity is selected.
+        if (activityError) {
+            activityTitle.style.color = ""; 
+            activityError.remove();
+        }
+      totalCost += price(item);
+      activitiesCost.innerHTML = 'Total: $' + totalCost;
+    } else if (e.target.checked == false) {
+      totalCost -= price(item);
+    }
+    activitiesCost.textContent = "Total: $" + totalCost;
+})
+
+
+//Payment section of my project.
+$('#paypal, #bitcoin').hide();
+
+//Set credit card as default method
+$('#payment').val("credit card");
+
+$('#payment').change(function(){
+	if ($('#payment option:selected').val() === "paypal") {
+		$('#credit-card, #bitcoin').hide();
+		$('#paypal').show();
+	} else if ($('#payment option:selected').val() === "bitcoin") {
+		$('#credit-card, #paypal').hide();
+		$('#bitcoin').show();
+	} else {
+		$('#credit-card').show();
+		$('#paypal, #bitcoin').hide();
+	}
 });
 
 
-var totalCost = 0;
 
 
-$(".activities").find("input:checkbox").change(function() {
-    $("#p2").remove();
 
-    //when you Select a checkbox for morning classes
-    if ($(this).prop("checked")) {
-      if ($(this).attr("id") == "morningBox") {
-        $(".activities > label").each(function() {
-          if ($(this).find("input").attr("id") == "morningBox") {
-            $(this).find("input:not(:checked)").attr("disabled", "disabled");
+function checkName(){
+  let nameField=$('#name').val();
+  let nameRegex=/^[a-zA-Z]+$/;
+  let Errname = false;
+  if (nameField == '' || !nameRegex.test(name))
+  namefield.after('<p id="nameError" class="error">Please  Enter Valid Name !!</p>');
+  else {
+    $('#nameError').remove();
+  }
+}
 
-            $(this).find("input:not(:checked)").parent().append();
-            // cost goes up by $100
-          }
-        });
-        totalCost += 100;
-      } else if ($(this).attr("id") == "afternoonBox") {
-        $(".activities > label").each(function() {
-          if ($(this).children().attr("id") === "afternoonBox") {
-            $(this).find("input:not(:checked)").attr("disabled", "disabled");
+function checkMail(){
+  let nameField=$('#mail').val();
+  let nameRegex=/^[a-zA-Z]+$/;
+  let Errname = false;
+  if (nameField == '' || !nameRegex.test(name))
+  namefield.after('<p id="nameError" class="error">Please  Enter Valid Name !!</p>');
+  else {
+    $('#nameError').remove();
+  }
+}
 
-            $(this).find("input:not(:checked)").parent().append();
-          }
-        });
-        totalCost += 100;
-      } else if ($(this).attr("id") == "wholeDayBox") {
-        //Main Conference
-        totalCost += 200;
-      }
-    } else if (!$(this).prop("checked")) {
-      if ($(this).attr("id") == "morningBox") {
-        $(".activities > label").each(function() {
-          if ($(this).find("input").attr("id") == "morningBox") {
-            $(this).find("input:not(:checked)").prop("disabled", false);
-            $(this).find("#Booked").remove();
-          }
-        });
-        //cost goes down $100
-        totalCost -= 100;
-      }
-      // if the checkbox related to a afternoon classes is unchecked
-      else if ($(this).attr("id") == "afternoonBox") {
-        $(".activities > label").each(function() {
-          if ($(this).children().attr("id") === "afternoonBox") {
-            $(this).find("input:not(:checked)").prop("disabled", false);
-            $(this).children("#Booked").remove();
-          }
-        });
-        // cost goes down $100
-        totalCost -= 100;
-      } else if ($(this).attr("id") == "wholeDayBox") {
-        // remaining box is for the conference
-        totalCost -= 200;
-      }
-    }
-    //update total cost
-    $(".activities").append("<p id='p2'>Total Cost Will Be: $" + totalCost + "</p>");
-  });
+function checkCredit(){
+  let nameField=$('#name').val();
+  let nameRegex=/^[a-zA-Z]+$/;
+  let Errname = false;
+  if (nameField == '' || !nameRegex.test(name))
+  namefield.after('<p id="nameError" class="error">Please  Enter Valid Name !!</p>');
+  else {
+    $('#nameError').remove();
+  }
+}
+
+function checkNameZip(){
+  let nameField=$('#name').val();
+  let nameRegex=/^[a-zA-Z]+$/;
+  let Errname = false;
+  if (nameField == '' || !nameRegex.test(name))
+  namefield.after('<p id="nameError" class="error">Please  Enter Valid Name !!</p>');
+  else {
+    $('#nameError').remove();
+  }
+}
+
+function checkCVV(){
+  let nameField=$('#name').val();
+  let nameRegex=/^[a-zA-Z]+$/;
+  let Errname = false;
+  if (nameField == '' || !nameRegex.test(name))
+  namefield.after('<p id="nameError" class="error">Please  Enter Valid Name !!</p>');
+  else {
+    $('#nameError').remove();
+  }
+}
